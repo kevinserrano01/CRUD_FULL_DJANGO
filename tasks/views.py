fa29c4185f2c0c1af8e5bf425cfe8ae0b642dd04
Kev <kevinserrano010@gmail.com>
2023-06-02 23:14:07 -0300

    Creamos la view para registrar un usuario y guardarlo en la base de datos.
    aclaro que primero valido si las contraseñas coinciden.

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm # formulario de registro
+from django.contrib.auth.models import User # Modelo de usuario
 from django.http import HttpResponse
 
 # Create your views here.
@@ -9,6 +10,22 @@ def home(request):
 
 def signup(request):
     """Permite registrar usuario"""
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+
+    if request.method == 'GET': # Envia formulario
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else: # Obteniendo datos del formulario
+        print('obteniendo datos')
+        if request.POST['password1'] == request.POST['password2']: # si contraseña1 y contraseña2 coinciden...
+            try:
+                # registrar usuario
+                nombre = request.POST['username'] # Save username
+                contraseña = request.POST['password1'] # Save password
+                usuario = User.objects.create_user(username=nombre, password=contraseña) # create user
+                usuario.save() # save user in database
+                return HttpResponse('Usuario creado exitosamente!')
+            except:
+                return HttpResponse('El usuario ya existe...')
+        else:
+            return HttpResponse('contraseñas no coinciden!')
