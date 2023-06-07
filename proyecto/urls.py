9c53fd80be737adab9f2cf37cde5aa96bef2679b
Kev <kevinserrano010@gmail.com>
2023-06-06 23:44:05 -0300

    Creacion de funcion para Iniciar Sesion y verificar antes si el username y passwod ya estan registrados en la base de datos.
    Creacion de template para iniciar sesion con su respectivo formulario.
    Login realizado!

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from tasks import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('logout/', views.signout, name='logout'),
+    path('admin/', admin.site.urls), # Admin
+    path('', views.home, name='home'), # Pagina Principal
+    path('signup/', views.signup, name='signup'), # Registrarse
+    path('tasks/', views.tasks, name='tasks'), # lista tareas
+    path('logout/', views.signout, name='logout'), # Cerrar Sesion
+    path('signin/', views.signin, name='signin'), # Iniciar Sesion
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm # formulario de registro
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# UserCreationForm: crear un usuario. | AuthenticationForm: comprobar si un usuario ya existe.
 from django.contrib.auth.models import User # Modelo de usuario
-from django.contrib.auth import login, logout # Crea cookie por nosotros
+from django.contrib.auth import login, logout, authenticate # Crea cookie por nosotros
 from django.db import IntegrityError
 
 
@@ -16,7 +17,7 @@ def signup(request):
             'form': UserCreationForm
         })
     else: # Obteniendo datos del formulario
-        print('obteniendo datos')
+        print('obteniendo datos de registro')
         if request.POST['password1'] == request.POST['password2']: # si contraseña1 y contraseña2 coinciden...
             try: # registrar usuario
                 nombre = request.POST['username'] # Save username
@@ -37,10 +38,45 @@ def signup(request):
                 })
 
 def tasks(request):
-    """Funcion que muestra la lista de tareas del usuario"""
+    """ Funcion que muestra la lista de tareas del usuario
+        Returns: Redirecciona a la lista de tareas
+    """
     return render(request, 'tasks.html')
 
 def signout(request):
     """Funcion que cierra sesion en la app y lo redirecciona al home"""
     logout(request)
     return redirect('home')
+
+def signin(request):
+    """Funcion para logearse (Iniciar sesion) en la app
+        Returns: Si no obtenemos datos solo recargamos el formulario, de lo contrario si recibimos, verificamos que ese user exista.
+        Si existe,
+    """
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        print('obteniendo datos de inicio de sesion')
+        # print(request.POST) # ver que trae username and password
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        # Verificar si la cuenta esta en la base de datos.
+        if user is None: # Si el user esta vacio me devuelve el formulario nuevamente con un mensaje de error.
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrecto'
+            })
+        else: # Si el usuario y contraseña existen, guarda sesion y lo redirecciono a la lista de tareas.
+            login(request, user) # Guardo sesion
+            return redirect('tasks') # Redireccion a lista de tareas
+
+        # Forma no necesaria ya que contamos con el authenticate del framework Django
+        # users = User.objects.all()
+        # print(users)
+        # for user in users:
+        #     if request.POST['username'] == user.username and request.POST['password'] == user.password:
+        #         print('usuario encontrado')
+        #         break
+        #     else:
+        #         print('user NO encontrado')
